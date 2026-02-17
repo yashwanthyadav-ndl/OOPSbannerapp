1c269ab138878713d9cade6b5e465f380c0e33a2
yashwanth s <yahwanthyadav0106@gmail.com>
2026-02-17 10:08:09 +0530

    Initial commit: add OOPSBannerapp Java file

---
+++ OOPSBannerapp.java
@@ -0,0 +1,15 @@
+
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * * This class demonstrates a simple Java application that displays the Object
+ * Oriented Programming System (OOPS) acronym to the console.
+ * * @author Developer
+ * @version 1.0
+ * @since 2026-02-17
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // This line prints the text to the console
+        System.out.println("OOPS");
+    }
+}

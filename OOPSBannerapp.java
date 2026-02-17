db742f75aa8a41f1b93a88d3d452733d2134c373
yashwanth s <yahwanthyadav0106@gmail.com>
2026-02-17 10:30:29 +0530

    UC1: print Hello World message

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,15 +1,6 @@
-
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- * * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System (OOPS) acronym to the console.
- * * @author Developer
- * @version 1.0
- * @since 2026-02-17
- */
-public class OOPSBannerApp {
+public class OOPSBannerapp {
     public static void main(String[] args) {
         // This line prints the text to the console
-        System.out.println("OOPS");
+        System.out.println("printing oops");
     }
 }

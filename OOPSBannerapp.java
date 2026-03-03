b6540ca07c622f884f8722985dbfef51be0581c9
yashwanth s <yahwanthyadav0106@gmail.com>
2026-03-03 08:57:06 +0530

    UC7: characterpattern

--- OOPSBannerapp.java
+++ OOPSBannerapp.java
@@ -1,28 +1,48 @@
-public class OOPSBannerapp {
+import java.util.*;
 
-    public static void main(String[] args) {
-        printBanner();
+public class BannerApp {
+    
+    // Map to store character patterns
+    private static final Map<Character, String[]> charMap = new HashMap<>();
+    
+    static {
+        charMap.put('O', new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        });
+        charMap.put('P', new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        });
+        charMap.put('S', new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        });
     }
-
-    // Function to return banner lines
-    public static String[] getBanner() {
-        return new String[] {
-                "    ***     ***    ******     *****   ",
-                "   ** **   ** **   **   **   **   **  ",
-                "  **   ** **   **  **   **   **        ",
-                "  **   ** **   **  ******      *****   ",
-                "  **   ** **   **  **              **  ",
-                "   ** **   ** **   **        **    **  ",
-                "    ***     ***     **         *****   "
-        };
-    }
-
-    // Function to print banner
-    public static void printBanner() {
-        String[] banner = getBanner();
-
-        for (String line : banner) {
+    
+    // Function to render a word
+    public static void renderWord(String word) {
+        word = word.toUpperCase();
+        for (int row = 0; row < 5; row++) {
+            StringBuilder line = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                String[] pattern = charMap.getOrDefault(ch, new String[]{"     ","     ","     ","     ","     "});
+                line.append(pattern[row]).append("  "); // spacing between letters
+            }
             System.out.println(line);
         }
+    }
+    
+    public static void main(String[] args) {
+        renderWord("OOPS");
     }
 }
